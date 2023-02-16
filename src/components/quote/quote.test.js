32265116ed14a09ed40ef14017ff1e933a2a24af
DenisPepper <thech@yandex.ru>
2023-02-16 22:04:10 +0300

    add test for selectRandomQuote

--- src/components/quote/quote.test.js
+++ src/components/quote/quote.test.js
@@ -2,35 +2,18 @@ import {render, screen} from "@testing-library/react";
 import {Quote} from "./quote";
 import {quotesList} from "./quotes-list";
 
-/**
- запустит перед каждым тестом функцию, которая подделает вызов (если он произойдет)
- Math.random() и вернет значение 1
- */
-beforeEach(() => {
-    jest.spyOn(global.Math, "random").mockReturnValue(0.8);
-})
-
-/**
- после каждого теста очистим моки для метода Math.random()
-
- если требуется очистить все моки: jest.restoreAllMocks()
- */
-afterEach(() => {
-    jest.spyOn(global.Math, "random").mockRestore(0.8);
-    /* jest.restoreAllMocks()  - очистит все моки*/
-});
-
 describe('test Quote FC', () => {
-    const {text, author} = quotesList[2];
+    const quoteStub = quotesList[2];
+    const {text, author} = quoteStub;
 
     it('should contain an expected quote text ', () => {
-        render(<Quote />);
+        render(<Quote quote={quoteStub}/>);
         const result = screen.getByText(new RegExp(text));
         expect(result).toBeInTheDocument();
     });
 
     it('should contain an expected author name ', () => {
-        render(<Quote />);
+        render(<Quote quote={quoteStub}/>);
         const result = screen.getByText(new RegExp(author));
         expect(result).toBeInTheDocument();
     });
