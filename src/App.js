4ad09f74acee5e751dce94ee249d3eeafcad1fd3
DenisPepper <thech@yandex.ru>
2023-02-16 11:52:05 +0300

    added components structure to App FC

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import * as css from './app.module.css';
 export const App = () => {
     return (
         <div className={css.main}>
-
+            {/* Header FC */}
+            {/* Converter FC */}
+            {/* Footer FC */}
         </div>
     );
-};
+}
