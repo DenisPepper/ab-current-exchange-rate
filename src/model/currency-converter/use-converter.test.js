bae40eea2552d602307370c1988f8de4246c2039
DenisPepper <thech@yandex.ru>
2023-02-18 10:43:21 +0300

    prepared draft tests of useConverter hook

---
+++ src/model/currency-converter/use-converter.test.js
@@ -0,0 +1,18 @@
+describe('when render', () => {
+    it.todo('check rub-input initial value');
+    it.todo('usd-input should be evaluated by rub initial value');
+})
+
+describe('when use updateRub', () => {
+    it.todo('should update the rub value');
+    it.todo('should evaluate the usd value');
+})
+
+describe('when use updateUsd', () => {
+    it.todo('should update the usd value');
+    it.todo('should evaluate the rub value');
+})
+
+describe('when re-render with new currency range', () => {
+    it.todo('should update input-value by new currency range');
+})
